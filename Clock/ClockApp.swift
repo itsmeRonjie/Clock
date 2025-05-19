463a9557a1f3ffe3b0ab9f1888861827d40aaa90
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-19 11:14:01 +0800

    - initialized project

---
+++ Clock/ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockApp.swift
+//  Clock
+//
+//  Created by Ronjie Diafante Man-on on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
