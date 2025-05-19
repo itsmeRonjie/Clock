53531cf47df107ae4c96d58781dcc6641f1ea95f
Ronjie Diafante Man-on <rdmanon@vauldex.com>
2025-05-19 14:03:19 +0800

    - added clock hand

--- Clock/View/Sub View/ClockView.swift
+++ Clock/View/Sub View/ClockView.swift
@@ -17,10 +17,38 @@ struct ClockView: View {
             size in
             let center = CGPoint(x: size.width / 2, y: size.height / 2)
             let calendar = Calendar.current
+            
             let seconds = calendar.component(.second, from: date)
             let minutes = calendar.component(.minute, from: date)
             let hours = calendar.component(.hour, from: date) % 12
             
+            func angle(for value: Int, unit: Int) -> Angle{
+                .degrees(Double(value) / Double(unit) * 360 - 90)
+            }
+            
+            let secondsAngle = angle(for: seconds, unit: 60)
+            let minutesAngle = angle(for: minutes, unit: 60 )
+            let hoursAngle = angle(for: hours * 5 + minutes / 12, unit: 60)
+            
+            func hand(at angle: Angle, length: CGFloat, color: Color) {
+                var path = Path()
+                let angleRadians: Double = angle.radians
+                
+                path.move(to: center)
+                let end = CGPoint(
+                    x: center.x + cos(Double(angleRadians)) * length,
+                    y: center.y + sin(angleRadians) * length
+                )
+                
+                path.addLine(to: end)
+                context
+                    .stroke(path, with: .color(color), lineWidth: 2)
+            }
+            
+            hand(at: hoursAngle, length: size.width * 0.25, color: .primary)
+            hand(at: minutesAngle, length: size.width * 0.35, color: .primary)
+            hand(at: secondsAngle, length: size.width * 0.4, color: .red)
+
             let centerCircle = Path(
                 ellipseIn: CGRect(
                     x: center.x - circleRadius,
